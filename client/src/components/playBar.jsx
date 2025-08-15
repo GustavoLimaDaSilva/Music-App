d68716194b2ed31bf213a8de90579a0f8d21d8b6
unknown <gustavolimadasilva50@gmail.com>
2025-08-15 19:45:12 -0300

    shuffle mode was added to playBar. PlayBar displays either ResizeWindow or ShuffleButton depending on the window size

--- client/src/components/playBar.jsx
+++ client/src/components/playBar.jsx
@@ -8,42 +8,51 @@ import { useEffect, useContext, useRef, useState } from "react"
 import { PlayerContext } from "../playerProvider"
 import { deviceAvailable, startTrack } from "../.."
 import { AuthContext } from "../authProvider"
+import useMobile from '../hooks/useMobile'
+import ShuffleButton from "./shuffleButton"
 
 export default function PlayBar() {
 
-    const { isPlaying, deviceID, queue } = useContext(PlayerContext)
+    const { isPlaying, deviceID, queue, playNext, setPlayNext, currentTrack } = useContext(PlayerContext)
     const { userCredentials } = useContext(AuthContext)
     const playBarRef = useRef(null)
+    const [isMobile, setIsMobile] = useMobile()
 
 
     useEffect(() => {
 
 
-        if (Object.keys(queue).length !== 0 && deviceID) {
+        if (Object.keys(queue).length !== 0 && deviceID && currentTrack) {
 
             playBarRef.current.classList.remove('hidden')
         }
     }, [deviceID, queue])
 
-    
+
+    useEffect(() => {
+        if (!currentTrack) return;
+
+        startTrack(deviceID, userCredentials.accessToken, currentTrack);
+        localStorage.setItem('currentTrack', JSON.stringify(currentTrack));
+
+    }, [currentTrack, deviceID, userCredentials.accessToken]);
+
 
     return (<>
-        {Object.keys(queue).length !== 0 ?
+        {currentTrack ?
             <div className="player-bar glass hidden" ref={playBarRef}>
                 <div className="flex-wrapper">
                     <div className="player-actions-bar">
                         <SkipPrevious />
-                        <PlayerButton songDuration={queue.list?.[queue.offset]?.duration_ms} />
+                        <PlayerButton songDuration={currentTrack.duration_ms} />
                         <SkipNext />
                     </div>
-                    <PlayerBarInfo currentTrack={queue.list[queue.offset]} />
+                    <PlayerBarInfo currentTrack={currentTrack} />
                     <div className="player-additional-tools">
-                        <ResizeWindow />
+                       {isMobile ? <ResizeWindow /> : <ShuffleButton />}
                     </div>
                 </div>
-
-                <ProgressBar track={queue.list[queue.offset]} />
-
+                <ProgressBar track={currentTrack} />
             </div> : null
         }
     </>)
