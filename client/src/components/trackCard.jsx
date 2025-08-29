edecb759c185b8c629e182a3f446d98f582fc939
unknown <gustavolimadasilva50@gmail.com>
2025-08-29 17:44:17 -0300

    refact(trackCard): Pass item to setTostream as an element inside an array

--- client/src/components/trackCard.jsx
+++ client/src/components/trackCard.jsx
@@ -9,7 +9,7 @@ export default function trackCard({ item }) {
     const [toStream, setToStream] = useStream()
 
     return (
-        <div onClick={isMobile ? () => setToStream(item) : null}>
+        <div onClick={isMobile ? () => setToStream([item]) : null}>
             <img src={item.album?.images?.[0].url} className={'card-img'} />
             <p className="card-title" onClick={!isMobile ? () => setToStream([item]): null}>{item.name}</p>
             <p className="card-subtitle">{isMobile ?
