4cdfca9804785f244741811dc7938aeab108d818
unknown <gustavolimadasilva50@gmail.com>
2025-09-05 16:25:38 -0300

    bugFix(shuffle): persist shuffle state and isolate its logic inside a hook. >> Now, with shuffle state being kept between different queues and page reloads, users can have a more seamless experience.

--- client/src/playerProvider.jsx
+++ client/src/playerProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState, useContext, useRef } from "react";
 import { AccountContext } from './App'
 import { AuthContext } from "./authProvider";
 import { createNewPlayer, startTrack } from "..";
+import useShuffle from "./hooks/useShuffle";
 
 export const PlayerContext = createContext(null)
 
@@ -15,10 +16,12 @@ export default function PlayerProvider({ children }) {
     const [isReady, setIsReady] = useState(false)
     const [deviceID, setDeviceID] = useState(null)
     const [isPlaying, setIsPlaying] = useState(false)
-
+    
+    
     const [playNext, setPlayNext] = useState([])
     const [queue, setQueue] = useState({})
     const [currentTrack, setCurrentTrack] = useState(JSON.parse(localStorage.getItem('currentTrack')))
+    const {isShuffle, setIsShuffle, triggerShuffle} = useShuffle(false, queue, setQueue)
 
     useEffect(() => {
 
@@ -45,7 +48,7 @@ export default function PlayerProvider({ children }) {
 
 
     return (
-        <PlayerContext.Provider value={{ deviceID, isReady, setIsReady, isPlaying, setIsPlaying, queue, setQueue, playNext, setPlayNext, currentTrack, setCurrentTrack }}>
+        <PlayerContext.Provider value={{ deviceID, isReady, setIsReady, isPlaying, setIsPlaying, queue, setQueue, playNext, setPlayNext, currentTrack, setCurrentTrack, isShuffle, setIsShuffle, triggerShuffle }}>
             {children}
         </PlayerContext.Provider>
     )
